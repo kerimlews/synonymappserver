97c52dc6bb480810173784903f18ed14ec62095d
kerimlews <kerimalihodza22@gmail.com>
2020-02-04 03:39:51 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var compression = require('compression');
+const path = require('path');
 const service = require('./utils/service');
 
 // port 
 const port = 3000
-
+const index = path.resolve(__dirname, '../', 'index.html')
 // app
 const app = express()
 
-
 // extensions
 app.use(cors())
+app.use(compression()); //Compress all routes
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // API
 
+app.get('/', (req, res, next) => res.sendFile(index))
 app.post('/add', service.add)
 app.get('/search/:word', service.search)
 
